92f992a23f65851707032db864f0e632e08f597d
Fede <f.gomezlara@outlook.es>
2021-11-22 15:16:01 +0100

    car model

---
+++ models/Car.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose')
+
+const carSchema = new Schema({
+    name: { type: String, required: true },
+    releaseDate: { type: Date, required: true },
+    rarity: [{ type: String, required: true }],
+    paintable: { type: Boolean, required: true, default: false },
+    unobtainablePaints: { type: String },
+    hitbox: { type: Schema.Types.ObjectId, ref: 'Hitbox' },
+    variantsIds: [{ type: String }],
+    description: { type: String, required: true },
+    availability: { type: String, required: true }
+})
+
+const Car = model('Car', carSchema)
+
+module.exports = Car
---
+++ models/Hitbox.model.js
@@ -0,0 +1,9 @@
+// const { Schema, model } = require('mongoose')
+
+// const hitboxSchema = new Schema({
+    
+// })
+
+// const Hitbox = model('Hitbox', hitboxSchema)
+
+// module.exports = Hitbox
