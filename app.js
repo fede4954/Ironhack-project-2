94fbf7eefa1e7c1c1ca94f97efbaa10bf6c7299f
Fede <f.gomezlara@outlook.es>
2021-11-22 13:43:10 +0100

    initial server setup

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+// require('./db')
+
+const express = require('express')
+const chalk = require('chalk')
+const hbs = require('hbs')
+const app = express()
+
+
+//MIDDLEWARE
+//View Engine
+app.set('views', __dirname + '/views')
+app.set('view engine', 'hbs')
+
+//Public folder
+app.use(express.static('public'))
+
+//Body parser
+app.use(express.json())
+
+
+//SERVER
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(chalk.bgGreen(`Server listening on port ${PORT}`))
+})
