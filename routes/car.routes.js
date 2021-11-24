aa6710b50078c61a2cbd713590b50a2993724928
Fede <f.gomezlara@outlook.es>
2021-11-24 16:03:31 +0100

    all cars view

--- routes/car.routes.js
+++ routes/car.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const chalk = require('chalk')
 
 //MODELS
 const Car = require('../models/Car.model')
@@ -7,14 +7,26 @@ const Car = require('../models/Car.model')
 
 //ROUTES
 //All cars page
-router.get('/', (req, res) => {
-    res.render('cars')
+router.get('/', async (req, res) => {
+    try{
+        const cars = await Car.find({})
+        res.render('cars', { cars })
+    }
+    catch(err){
+        console.log(chalk.bgRed('Error loading all cars:', err))
+    }
 })
 
 //Specific car page
-router.get('/:id', (req, res) => {
-    const car = Car.findById(req.params.id)
-    res.render('carInfo', { car })
-})
+// router.get('/:id', (req, res) => {
+//     try{
+//         const car = Car.findById(req.params.id)
+//         res.render('carInfo', { car })
+//     }
+//     catch(err){
+//         console.log(chalk.bgRed(`Error loading car (${car.name}):`, err))
+//     }
+    
+// })
 
 module.exports = router
