1bbd58fa5c27c95067041e4805070d7f77ea9bce
Fede <f.gomezlara@outlook.es>
2022-01-12 17:00:57 +0100

    delete unused hitbox model, update car model

--- models/Car.model.js
+++ models/Car.model.js
@@ -6,10 +6,11 @@ const carSchema = new Schema({
     releaseDate: { type: String, required: true },
     rarity: [{ type: String, required: true }],
     paintable: { type: Boolean, default: false },
-    hitbox: { type: Schema.Types.ObjectId, ref: 'Hitbox' },
+    hitbox: { type: String, ref: 'Hitbox' },
     variants: [{ type: String }],
     description: { type: String, required: true },
-    timesFavorited: { type: Number, default: 0, min: 0}
+    wikiLink: { type: String },
+    timesFavorited: { type: Number, default: 0, min: 0 }
 })
 
 const Car = model('Car', carSchema)
